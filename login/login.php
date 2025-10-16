5792d7112f7104625296a219f8924ac324f91d6e
asantx <yaaasantewaaasante2003@gmail.com>
2025-10-16 17:12:04 +0000

    Updated navigation

--- login/login.php
+++ login/login.php
@@ -135,5 +135,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="../js/login.js"></script>
+    <script src="../js/index.js"></script>
 </body>
 </html>
