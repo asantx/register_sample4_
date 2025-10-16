74eeed1e905674e2df1694899c57547676d2d112
asantx <yaaasantewaaasante2003@gmail.com>
2025-10-16 17:24:39 +0000

    Updated navigation

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -106,7 +106,7 @@ requireAdmin('../login/login.php');
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="/js/index.js"></script>
+    <script src="/js/reference.js"></script>
 </body>
 
 </html>
