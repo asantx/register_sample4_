173cbbf6cd5c2d180ad82e56a5aec00a533de168
asantx <yaaasantewaaasante2003@gmail.com>
2025-10-23 19:15:11 +0000

    Updated

--- login/login.php
+++ login/login.php
@@ -134,7 +134,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="../js/app_root.js"></script>
+    <!-- <script src="../js/app_root.js"></script> -->
     <script src="../js/login.js"></script>
     <script src="../js/index.js"></script>
 </body>
