f35ec364e8f06c27bf688bad71cf30fd46906bd9
asantx <yaaasantewaaasante2003@gmail.com>
2025-11-18 20:15:08 +0000

    Fix: Correct path for orders.js inclusion in orders.php

--- views/orders.php
+++ views/orders.php
@@ -243,6 +243,6 @@ requireLogin();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="js/orders.js"></script>
+    <script src="../js/orders.js"></script>
 </body>
 </html>
