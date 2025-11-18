1c2a648e22411f2afa9da317a4ba66cda8ebb924
asantx <yaaasantewaaasante2003@gmail.com>
2025-11-18 17:39:42 +0000

    Update page titles and navbar branding from 'Shoppn' to 'DistantLove'

--- views/cart.php
+++ views/cart.php
@@ -8,7 +8,7 @@ requireLogin();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cart - Shoppn</title>
+    <title>Cart - DistantLove</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
@@ -224,7 +224,7 @@ requireLogin();
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Shoppn</a>
+            <a class="navbar-brand" href="index.php">DistantLove</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
